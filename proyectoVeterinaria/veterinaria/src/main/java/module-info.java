33d62496e2289826b844f222dc2c1005b7dbe82f
SebastianMartinezI <juans.martinezi@uqvirtual.edu.co>
2025-08-11 19:59:59 -0500

    Ya abre la interfaz del App, solo falta hacer que conecte con las otras interfaces
    Faltaba modificar el module-info.java para poder lanzar la ejecución del App, se logró efectivamente, falta poco

--- proyectoVeterinaria/veterinaria/src/main/java/module-info.java
+++ proyectoVeterinaria/veterinaria/src/main/java/module-info.java
@@ -2,7 +2,9 @@ module co.edu.uniquindio.poo.veterinaria {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens co.edu.uniquindio.poo.veterinaria to javafx.fxml;
     exports co.edu.uniquindio.poo.veterinaria;
+    exports co.edu.uniquindio.poo.veterinaria.viewController;
+    opens co.edu.uniquindio.poo.veterinaria.viewController to javafx.fxml;
+
 }
